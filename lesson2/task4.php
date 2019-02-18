f1defa9fa6d476e3656126c04f1e1257aad4371f
valentina-aktanko <freyakhv@gmail.com>
2019-02-18 21:38:30 +1000

    lesson2, task4

--- lesson2/task4.php
+++ lesson2/task4.php
@@ -1,7 +1,47 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Валя
- * Date: 18.02.2019
- * Time: 18:02
- */
+
+function add($arg1, $arg2)
+{
+    return $arg1 + $arg2;
+}
+
+function subtraction($arg1, $arg2)
+{
+    return $arg1 - $arg2;
+}
+
+function multiply($arg1, $arg2)
+{
+    return $arg1 * $arg2;
+}
+
+function division($arg1, $arg2)
+{
+    return (($arg2 !== 0) ? ($arg1 / $arg2) : ("Ошибка! Деление на 0")); // Скобки для наглядности
+}
+
+function mathOperation($arg1, $arg2, $operation)
+{
+    switch ($operation) {
+        case "add":
+            echo("Сумма чисел $arg1 и $arg2: ");
+            echo(add($arg1, $arg2));
+            break;
+        case "subtraction":
+            echo("Разность чисел $arg1 и $arg2: ");
+            echo(subtraction($arg1, $arg2));
+            break;
+        case "multiply":
+            echo("Произведение чисел $arg1 и $arg2: ");
+            echo(multiply($arg1, $arg2));
+            break;
+        case "division":
+            echo("Результат деления числа $arg1 на число $arg2: ");
+            echo(division($arg1, $arg2));
+            break;
+        default:
+            echo("$operation - Неизвестная операция");
+    }
+}
+
+mathOperation(10, 0,"division");
