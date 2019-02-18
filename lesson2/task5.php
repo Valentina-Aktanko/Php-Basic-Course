e96242cc5ca04093694be1a0ef8654c5ef6479d3
valentina-aktanko <freyakhv@gmail.com>
2019-02-18 22:35:58 +1000

    lesson2, task5

--- lesson2/task5.php
+++ lesson2/task5.php
@@ -1,7 +1,13 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Валя
- * Date: 18.02.2019
- * Time: 22:17
- */
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<footer><?='Текущий год - ' . date("Y")?></footer>
+</body>
+</html>
