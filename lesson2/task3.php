8cd8bbd8ee510e908fc82a023c17c5ace263d420
valentina-aktanko <freyakhv@gmail.com>
2019-02-18 17:52:01 +1000

    lesson2 task3

--- lesson2/task3.php
+++ lesson2/task3.php
@@ -1,7 +1,32 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Валя
- * Date: 18.02.2019
- * Time: 14:33
- */
+
+function add($arg1, $arg2) {
+    return $arg1 + $arg2;
+}
+
+function subtraction($arg1, $arg2) {
+    return $arg1 - $arg2;
+}
+
+function multiply ($arg1, $arg2) {
+    return $arg1 * $arg2;
+}
+
+function division($arg1, $arg2) {
+    return $arg1 / $arg2;
+}
+
+$arg1 = 10;
+$arg2 = 2;
+
+echo("Сумма чисел $arg1 и $arg2: ");
+echo(add($arg1, $arg2). "<br>");
+
+echo("Разность чисел $arg1 и $arg2: ");
+echo(subtraction($arg1, $arg2). "<br>");
+
+echo("Произведение чисел $arg1 и $arg2: ");
+echo(multiply($arg1, $arg2). "<br>");
+
+echo("Частное чисел $arg1 и $arg2: ");
+echo(division($arg1, $arg2). "<br>");
