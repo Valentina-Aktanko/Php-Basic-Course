0a588fb92d19f15ebcf41d16ca77bbc12e18aad4
valentina-aktanko <freyakhv@gmail.com>
2019-02-24 21:01:27 +1000

    lesson3, task2, cycle Do while

--- lesson3/task2.php
+++ lesson3/task2.php
@@ -1,7 +1,14 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Валя
- * Date: 24.02.2019
- * Time: 20:40
- */
+
+$number = 0;
+
+do {
+    if ($number === 0) {
+        echo "$number - это ноль<br>";
+    } elseif ($number % 2 === 0) {
+        echo "$number - это чётное число<br>";
+    } else {
+        echo "$number - это нечётное число<br>";
+    }
+    $number++;
+} while ($number <= 10);
