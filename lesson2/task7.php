acd668fb130ea9955cc4ec63b501d517aa8897f2
valentina-aktanko <freyakhv@gmail.com>
2019-02-24 15:39:27 +1000

    lesson2, task7, solution without using an array

--- lesson2/task7.php
+++ lesson2/task7.php
@@ -1,33 +1,35 @@
 <?php
 
 // час: 1
-// часа: 2, 3, 4,
-// часов: 5, 6, 7, 8, 9, 0, 11, 12, 13, 14,
+// часа: 2, 3, 4
+// часов: 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
 
-// минут: 5, 6, 7, 8, 9, 0
-// минута: 1,
-// минуты: 2, 3, 4,
+// минута: 1
+// минуты: 2, 3, 4
+// минут: 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
 
-function getDeclension($number){
-    switch ((int)substr($number, -1)) {
-        case 1:
-            return ['час', 'минута'];
-        case 2:
-        case 3:
-        case 4:
-            return ['часа', 'минуты'];
-        case 5:
-        case 6:
-        case 7:
-        case 8:
-        case 9:
-        case 0:
-            return ['часов', 'минут'];
-        default: ['-', '-'];
+
+function timeNow($hour, $minute){
+
+    $result = "";
+
+    if($hour%100 == 0 || ($hour%100 >= 5 && $hour%100 <= 20)) {
+        $result .= $hour.' часов ';
+    } elseif ($hour%10 == 1){
+        $result .= $hour.' час ';
+    } else {
+        $result .= $hour.' часа ';
     }
+
+    if($minute%10 == 0 || ($minute%10 >= 5 && $hour%10 <= 9) || ($minute%100 >= 11 && $minute%100 <=14)) {
+        $result .= $minute.' минут';
+    } elseif ($minute%10 == 1){
+        $result .= $minute.' минута';
+    } else {
+        $result .= $minute.' минуты';
+    }
+
+    return $result;
 }
 
-$hour = Date("h");
-$minute = Date("i");
-
-echo("Сейчас $hour " . getDeclension($hour)[0] . " $minute " . getDeclension($minute)[1]);
+echo("Сейчас " . timeNow(date("G"), date("i")));
