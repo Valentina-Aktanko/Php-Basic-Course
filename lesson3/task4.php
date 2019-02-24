7c165ab842e86d05d9387f3321e1db137de8ebe7
valentina-aktanko <freyakhv@gmail.com>
2019-02-24 23:38:25 +1000

    added array_key_exists check

--- lesson3/task4.php
+++ lesson3/task4.php
@@ -1,56 +1,57 @@
 <?php
 
-$letters = [
-    "а" => "a",
-    "б" => "b",
-    "в" => "v",
-    "г" => "g",
-    "д" => "d",
-    "е" => "e",
-    "ё" => "yo",
-    "ж" => "zh",
-    "з" => "z",
-    "и" => "i",
-    "й" => "j",
-    "к" => "k",
-    "л" => "l",
-    "м" => "m",
-    "н" => "n",
-    "о" => "o",
-    "п" => "p",
-    "р" => "r",
-    "с" => "s",
-    "т" => "t",
-    "у" => "u",
-    "ф" => "f",
-    "х" => "kh",
-    "ц" => "c",
-    "ч" => "ch",
-    "ш" => "sh",
-    "щ" => "shch",
-    "ъ" => "``",
-    "ы" => "y",
-    "ь" => "`",
-    "э" => "e",
-    "ю" => "yu",
-    "я" => "ya",
-    "" => ""
-];
+function toTranslate($string) {
 
-function toTranslate($string, $letters) {
+    $letters = [
+        "а" => "a",
+        "б" => "b",
+        "в" => "v",
+        "г" => "g",
+        "д" => "d",
+        "е" => "e",
+        "ё" => "yo",
+        "ж" => "zh",
+        "з" => "z",
+        "и" => "i",
+        "й" => "j",
+        "к" => "k",
+        "л" => "l",
+        "м" => "m",
+        "н" => "n",
+        "о" => "o",
+        "п" => "p",
+        "р" => "r",
+        "с" => "s",
+        "т" => "t",
+        "у" => "u",
+        "ф" => "f",
+        "х" => "kh",
+        "ц" => "c",
+        "ч" => "ch",
+        "ш" => "sh",
+        "щ" => "shch",
+        "ъ" => "``",
+        "ы" => "y",
+        "ь" => "`",
+        "э" => "e",
+        "ю" => "yu",
+        "я" => "ya",
+        "" => ""
+    ];
 
     $newString = "";
-    $encoding = mb_detect_encoding($string); // получаем кодировку
+    $encoding = mb_detect_encoding($string); // получаем кодировку строки
     $strCount =  mb_strlen($string, $encoding); // определяем длину строки в соответствующей кодировке
 
     for ($i = 0; $i < $strCount; $i++) {
         $symbol = mb_substr($string, $i, 1); // получаем символ строки по индексу
         $symbol = mb_strtolower($symbol); // преобразуем символ в нижний регистр
-
-        $newString .= $letters[$symbol]; // выполняем транслитерацию
+        if (array_key_exists($symbol, $letters)) { // если ключ найден
+            $newString .= $letters[$symbol]; // выполняем транслитерацию
+        }
     }
 
     return $newString;
 }
 
-echo toTranslate("КОРова", $letters);
+echo toTranslate("КОРовa");
