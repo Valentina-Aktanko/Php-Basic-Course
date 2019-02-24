9b2b858740aa9b3b92ce7cda5ef913548ab69304
valentina-aktanko <freyakhv@gmail.com>
2019-02-24 22:55:42 +1000

    lesson3, task4, associative array, cyrillistic keys

--- lesson3/task4.php
+++ lesson3/task4.php
@@ -1,7 +1,56 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Валя
- * Date: 24.02.2019
- * Time: 21:26
- */
+
+$letters = [
+    "а" => "a",
+    "б" => "b",
+    "в" => "v",
+    "г" => "g",
+    "д" => "d",
+    "е" => "e",
+    "ё" => "yo",
+    "ж" => "zh",
+    "з" => "z",
+    "и" => "i",
+    "й" => "j",
+    "к" => "k",
+    "л" => "l",
+    "м" => "m",
+    "н" => "n",
+    "о" => "o",
+    "п" => "p",
+    "р" => "r",
+    "с" => "s",
+    "т" => "t",
+    "у" => "u",
+    "ф" => "f",
+    "х" => "kh",
+    "ц" => "c",
+    "ч" => "ch",
+    "ш" => "sh",
+    "щ" => "shch",
+    "ъ" => "``",
+    "ы" => "y",
+    "ь" => "`",
+    "э" => "e",
+    "ю" => "yu",
+    "я" => "ya",
+    "" => ""
+];
+
+function toTranslate($string, $letters) {
+
+    $newString = "";
+    $encoding = mb_detect_encoding($string); // получаем кодировку
+    $strCount =  mb_strlen($string, $encoding); // определяем длину строки в соответствующей кодировке
+
+    for ($i = 0; $i < $strCount; $i++) {
+        $symbol = mb_substr($string, $i, 1); // получаем символ строки по индексу
+        $symbol = mb_strtolower($symbol); // преобразуем символ в нижний регистр
+
+        $newString .= $letters[$symbol]; // выполняем транслитерацию
+    }
+
+    return $newString;
+}
+
+echo toTranslate("КОРова", $letters);
