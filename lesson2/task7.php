b9aa6adce6629e3f56748bf7641a6f76e55a92cd
valentina-aktanko <freyakhv@gmail.com>
2019-02-24 20:29:31 +1000

    lesson2, task7, second option

--- lesson2/task7.php
+++ lesson2/task7.php
@@ -8,7 +8,7 @@
 // минуты: 2, 3, 4
 // минут: 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
 
-
+// первый вариант
 function timeNow($hour, $minute){
 
     $result = "";
@@ -32,4 +32,37 @@ function timeNow($hour, $minute){
     return $result;
 }
 
+// второй вариант
+function getWordDeclension ($n, $one, $two, $five) {
+    $n100 = $n % 100;
+    if ($n100 > 10 && $n100 < 20) {
+        return "$n $five";
+    }
+
+    $n10 = $n % 10;
+    if ($n10 === 1) {
+        return "$n $one";
+    }
+
+    if ($n10 >= 2 && $n10 <= 4) {
+        return "$n $two";
+    }
+
+    return "$n $five";
+}
+
+// первый вариант
 echo("Сейчас " . timeNow(date("G"), date("i")));
+echo "<br>";
+echo "<hr>";
+
+// второй вариант
+$h = rand(0, 23);
+echo getWordDeclension($h, "час", "часа", "часов") . " ";
+$m = rand(0, 59);
+echo getWordDeclension($m, "минута", "минуты", "минут");
+
+// универсальная функция
+echo "<br>";
+echo "<hr>";
+echo getWordDeclension(rand(0, 99999999), "корова", "коровы", "коров");
