526221426be357b91b07f8e7dec1c4135d875538
valentina-aktanko <freyakhv@gmail.com>
2019-02-25 21:07:02 +1000

    lesson3, task8, display cities from the array, which begin with the letters K

--- lesson3/task8.php
+++ lesson3/task8.php
@@ -1,7 +1,24 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Валя
- * Date: 25.02.2019
- * Time: 20:33
- */
+
+$regions = [
+    "Московская область" => ["Москва", "Зеленоград", "Клин"],
+    "Ленинградская  область" => ["Санкт-Петербур", "Всеволожск", "Павловск", "Кронштадт"],
+    "Рязанская  область" => ["Рязань", "Рыбное", "Касимов"],
+];
+
+$str = "";
+
+foreach ($regions as $region => $towns) {
+    echo "$region:<br>";
+    foreach ($towns as $town) {
+        $firstLetter = mb_strtoupper(mb_substr($town, 0, 1));
+        if (strcasecmp($firstLetter, "К") === 0) {
+            $str .= $town . ", ";
+        }
+    }
+
+    $encoding = mb_detect_encoding($str);
+    $strCount =  mb_strlen($str, $encoding);
+    echo mb_substr($str, 0, $strCount -2) . "<br>";
+    $str = "";
+}
