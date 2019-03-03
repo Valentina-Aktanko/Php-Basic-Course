ec8e0b66d216eb502712bfd0a645d4f9ff048cf8
valentina-aktanko <freyakhv@gmail.com>
2019-03-03 12:45:49 +1000

    revised lesson3 task8

--- lesson3/task8.php
+++ lesson3/task8.php
@@ -1,24 +1,17 @@
 <?php
 
 $regions = [
-    "Московская область" => ["Москва", "Зеленоград", "Клин"],
-    "Ленинградская  область" => ["Санкт-Петербур", "Всеволожск", "Павловск", "Кронштадт"],
-    "Рязанская  область" => ["Рязань", "Рыбное", "Касимов"],
+    'Московская область' => ['Москва', 'Зеленоград', 'Клин'],
+    'Ленинградская  область' => ['Санкт-Петербур', 'Всеволожск', 'Павловск', 'Кронштадт'],
+    'Рязанская  область' => ['Рязань', 'Рыбное', 'Касимов'],
 ];
 
-$str = "";
-
 foreach ($regions as $region => $towns) {
-    echo "$region:<br>";
+    echo "$region: <br>";
     foreach ($towns as $town) {
-        $firstLetter = mb_strtoupper(mb_substr($town, 0, 1));
-        if (strcasecmp($firstLetter, "К") === 0) {
-            $str .= $town . ", ";
-        }
+       if (mb_substr($town, 0, 1) == 'К') {
+           echo $town . ' ';
+       }
     }
-
-    $encoding = mb_detect_encoding($str);
-    $strCount =  mb_strlen($str, $encoding);
-    echo mb_substr($str, 0, $strCount -2) . "<br>";
-    $str = "";
+   echo '<br>';
 }
