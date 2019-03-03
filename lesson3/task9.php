836adf23ab983d47348ae86c3620f02ff6134c05
valentina-aktanko <freyakhv@gmail.com>
2019-03-03 12:51:08 +1000

    revised lesson3 task9

--- lesson3/task9.php
+++ lesson3/task9.php
@@ -1,62 +1,24 @@
 <?php
 
-function toTranslate($string) {
+function toTranslate($str) {
 
     $letters = [
-        "а" => "a",
-        "б" => "b",
-        "в" => "v",
-        "г" => "g",
-        "д" => "d",
-        "е" => "e",
-        "ё" => "yo",
-        "ж" => "zh",
-        "з" => "z",
-        "и" => "i",
-        "й" => "j",
-        "к" => "k",
-        "л" => "l",
-        "м" => "m",
-        "н" => "n",
-        "о" => "o",
-        "п" => "p",
-        "р" => "r",
-        "с" => "s",
-        "т" => "t",
-        "у" => "u",
-        "ф" => "f",
-        "х" => "kh",
-        "ц" => "c",
-        "ч" => "ch",
-        "ш" => "sh",
-        "щ" => "shch",
-        "ъ" => "``",
-        "ы" => "y",
-        "ь" => "`",
-        "э" => "e",
-        "ю" => "yu",
-        "я" => "ya",
-        " " => " ",
+        'а' => 'a', 'б' => 'b', 'в' => 'v', 'г' => 'g', 'д' => 'd', 'е' => 'e', 'ё' => 'yo', 'ж' => 'zh', 'з' => 'z',
+        'и' => 'i', 'й' => 'j', 'к' => 'k', 'л' => 'l', 'м' => 'm', 'н' => 'n', 'о' => 'o', 'п' => 'p', 'р' => 'r',
+        'с' => 's', 'т' => 't', 'у' => 'u', 'ф' => 'f', 'х' => 'kh', 'ц' => 'c', 'ч' => 'ch', 'ш' => 'sh', 'щ' => 'shch',
+        'ъ' => '"', 'ы' => 'y', 'ь' => '\'', 'э' => 'ie', 'ю' => 'yu', 'я' => 'ya'
     ];
 
-    $newString = "";
-    $encoding = mb_detect_encoding($string); // получаем кодировку строки
-    $strCount =  mb_strlen($string, $encoding); // определяем длину строки в соответствующей кодировке
+    $str = mb_strtolower($str);
+    $res = strtr($str, $letters);
 
-    for ($i = 0; $i < $strCount; $i++) {
-        $symbol = mb_substr($string, $i, 1); // получаем символ строки по индексу
-        $symbol = mb_strtolower($symbol); // преобразуем символ в нижний регистр
-        if (array_key_exists($symbol, $letters)) { // если ключ найден
-            $newString .= $letters[$symbol]; // выполняем транслитерацию
-        }
-    }
-
-    return $newString;
+    return $res;
 }
 
-function withSpaces($string) {
-    return str_replace(" ", "_", $string);
+function replace($str) {
+    return str_replace(" ", "_", $str);
 }
 
-echo withSpaces(toTranslate("Строка с пробелами"));
+$str = 'Строка с пробелами';
+echo replace(toTranslate($str));
 
