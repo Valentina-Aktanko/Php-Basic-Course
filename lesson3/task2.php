764aa456fdf74d292a1e6ad921b9c489c63bb8d1
valentina-aktanko <freyakhv@gmail.com>
2019-03-03 11:15:50 +1000

    revised lesson3 task2

--- lesson3/task2.php
+++ lesson3/task2.php
@@ -3,9 +3,9 @@
 $number = 0;
 
 do {
-    if ($number === 0) {
+    if ($number == 0) {
         echo "$number - это ноль<br>";
-    } elseif ($number % 2 === 0) {
+    } elseif ($number > 0 && $number%2 == 0) {
         echo "$number - это чётное число<br>";
     } else {
         echo "$number - это нечётное число<br>";
